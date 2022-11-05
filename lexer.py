e86088d50964d68fd97228e5022e1a910ea1e4a5
InAnYan <ruslanpopov1512@gmail.com>
2022-11-05 21:21:41 +0200

    start

---
+++ lexer.py
@@ -0,0 +1,21 @@
+class Lexer:
+    def __init__(self, input):
+        pass
+
+    def next_char(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def abort(self, message):
+        pass
+
+    def skip_whitespaces(self):
+        pass
+
+    def skip_comment(self):
+        pass
+
+    def get_token(self):
+        pass
